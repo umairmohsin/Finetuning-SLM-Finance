702cacb235da571286412b5c11a0826a35fc7955
Muhammad Umair Mohsin <77578635+umairmohsin@users.noreply.github.com>
2026-01-05 02:08:56 +0300

    Update test_base.py

--- test_base.py
+++ test_base.py
@@ -1,7 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-# model_name = "Qwen/Qwen3-0.6B"
 model_name = r"D:\Models\Qwen3-0.6B"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -20,3 +19,4 @@ inputs = tokenizer(prompt, return_tensors="pt")
 outputs = model.generate(**inputs, max_new_tokens=80)
 
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
